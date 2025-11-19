5a4218013b99348116682553d011c812ebd48637
THIAGO MARTINS FERNANDES <52598542843@edu.udesc.br>
2025-11-19 11:20:12 -0300

    adicionei tal coisa, teste

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ v_sg, v_m, v_sl, rho_g, rho_l, p_atm, P, g, d_h, sigma_l, theta = 1, 1, 1, 1, 1,
 if __name__ == "__main__":
     v_lsc = 6000 / 86400
     theta_1 = 15 #Trecho tubulação - reservatório
-    
+    #hdh#
