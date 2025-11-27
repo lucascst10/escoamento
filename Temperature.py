28e49423f4619d4ceab0d076a6fcc60bd10bbeb2
JoaoVassalo <joaodvassalo@gmail.com>
2025-11-26 21:02:37 -0300

    Cálculo de temperatura.

---
+++ Temperature.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def calc_Temp(
+        dl,
+        C_p,
+        m_gas,
+        m_liquido,
+        T_0        
+
+):
+    g = 9.81 #m/s²
+
+    #Cálculo da temperatura externa
+    if (dl < 579.55):
+        T_inf = 80 - 0.1277*dl
+        theta_rad = 10*np.pi/180 #rad
+        TEC = 2 #W/mK
+    elif (dl < 867.49):
+        T_inf = 80 - 0.1277*579.55
+        theta_rad = 15*np.pi/180 #rad
+        TEC = 1 #W/mK 
+    else:
+        T_inf = 6 + 0.01*dl
+        theta_rad = 90*np.pi/180 #rad
+        TEC = 1 #W/mK
+
+    m_total = m_gas + m_liquido
+
+    #T_0 é a temperatura anterior, a inicial no poço é igual à 80°C. 
+    T = T_inf - m_total*g*np.sin(theta_rad)/TEC - np.exp((-TEC*dl)/(m_total*C_p)) * (T_inf - (m_total*g*np.sin(theta_rad)/TEC) - T_0)
+    
+    return T
