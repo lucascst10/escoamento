984b006ef14dcb608d96119bf62b29cc645cb718
thigass97 <thi27martins@gmail.com>
2025-11-27 22:08:06 -0300

    Update Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -6,15 +6,15 @@ def calc_Temp(dl, m_total, T_0):
 
     if dl <= 579.55:
         T_inf = 80 - 0.1277 * dl
-        theta_rad = 10 * np.pi / 180
+        theta_rad = 10 
         TEC = 2
     elif dl <= 867.49:
         T_inf = 6
-        theta_rad = 15 * np.pi / 180
+        theta_rad = 15 
         TEC = 1
     else:
         T_inf = 6 + 0.01 * dl
-        theta_rad = 90 * np.pi / 180
+        theta_rad = 90 
         TEC = 1
 
     C_p = ((2*10**(-3)*T_0 - 1.429)*SG + (2.67*10**(-3))*T_0 + 3.049) * 10**(3)
